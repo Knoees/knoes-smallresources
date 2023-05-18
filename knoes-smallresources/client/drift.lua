f4f3d90d61c7722b147840a686262baf6074a3a2
Pumpkin Shop <119126465+Knoees@users.noreply.github.com>
2023-05-18 16:37:45 +0300

    Update drift.lua

--- knoes-smallresources/client/drift.lua
+++ knoes-smallresources/client/drift.lua
@@ -1,36 +1,38 @@
 local kmh, mph = 3.6, 2.23693629
 local carSpeed = 0
-local speed = kmh -- veya mph
-local speedLimit = 100.0
-local driftMode = false
+local speed = kmh -- or mph
+local speedLimit = 1000.0 
+local driftMode
 
-local function StartDriftMode(drift)
-	while drift do
+-- Thread
+CreateThread(function()
+	while true do
 		Wait(250)
-		if IsPedInAnyVehicle(PlayerPedId(), false) then
-			carSpeed = GetEntitySpeed(GetVehiclePedIsIn(PlayerPedId())) * speed
-			if GetPedInVehicleSeat(GetVehiclePedIsIn(PlayerPedId(), false), -1) == PlayerPedId() then
-				if (carSpeed <= speedLimit) then  
-					if IsControlPressed(0, 21) then
-						SetVehicleReduceGrip(GetVehiclePedIsIn(PlayerPedId(), false), true)
-					else
-						SetVehicleReduceGrip(GetVehiclePedIsIn(PlayerPedId(), false), false)
+		if not driftMode then
+			if IsPedInAnyVehicle(PlayerPedId(), false) then
+				carSpeed = GetEntitySpeed(GetVehiclePedIsIn(PlayerPedId())) * speed
+				if GetPedInVehicleSeat(GetVehiclePedIsIn(PlayerPedId(), false), -1) == PlayerPedId() then
+					if (carSpeed <= speedLimit) then  
+						if IsControlPressed(0, 21) then
+							SetVehicleReduceGrip(GetVehiclePedIsIn(PlayerPedId(), false), true)
+						else
+							SetVehicleReduceGrip(GetVehiclePedIsIn(PlayerPedId(), false), false)
+						end
 					end
 				end
 			end
 		end
-		if not driftMode then break end
-	end
-end
-
-RegisterCommand('driftmode', function()
-	driftMode = not driftMode
-	if driftMode then
-		SetVehicleReduceGrip(GetVehiclePedIsIn(PlayerPedId(), false), false)
-		StartDriftMode(true)
-	else
-		SetVehicleReduceGrip(GetVehiclePedIsIn(PlayerPedId(), false), true)
-		StartDriftMode(false)
 	end
 end)
+
+RegisterCommand('driftmode', function()
+	if not driftMode then
+		SetVehicleReduceGrip(GetVehiclePedIsIn(PlayerPedId(), false), true)
+		driftMode = true
+	else
+		SetVehicleReduceGrip(GetVehiclePedIsIn(PlayerPedId(), false), false)
+		driftMode = false
+	end
+end)
+
 
